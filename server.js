5c795477662c9a2c62239ebec239ebd4ebc36a40
Arshdeep Singh Chhabra <arshdeepchhabra1712@gmail.com>
2022-12-06 15:07:16 +0530

    files added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const connection = require('./database');
 
 const app = express();
 const port = 3700;
@@ -12,7 +13,19 @@ app.get("/", function(req,res){
 
 app.post("/submit", function(req,res){
      console.log(req.body)
-     res.send("Done")
+     let name = req.body.name
+     let email = req.body.email
+     let contact = req.body.contact
+
+     connection.connect(function(err){
+        if(err) throw err
+        let sql = "INSERT INTO form(name, email, contact) VALUES(?,?,?)"; 
+        connection.query(sql,[name, email,contact], function(err, result){
+            if(err) throw err
+            res.send("Successfully submitted to database !")
+        })
+    })
+    
 })
 
 
