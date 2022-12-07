6ddc0c965219d52aef0f941de855ec66939dbed1
Arshdeep Singh Chhabra <arshdeepchhabra1712@gmail.com>
2022-12-07 18:06:21 +0530

    Files added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const connection = require('./database');
 
 const app = express();
@@ -7,12 +8,15 @@ const port = 3700;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
+app.set('view engine', 'ejs') // here we will set our router to display data
+
 app.get("/", function(req,res){
     res.sendFile(__dirname + "/index.html")
 })
 
 app.post("/submit", function(req,res){
-     console.log(req.body)
+    //  console.log(req.body)
      let name = req.body.name
      let email = req.body.email
      let contact = req.body.contact
@@ -23,10 +27,46 @@ app.post("/submit", function(req,res){
         connection.query(sql,[name, email,contact], function(err, result){
             if(err) throw err
             res.send("Successfully submitted to database !")
+            
         })
     })
     
 })
+app.get("/fetch-data", (req,res)=>{
+
+    connection.connect(function(err){
+        if(err) throw err
+        let sql = "select * from form";
+        connection.query(sql,function(err, result){
+            if(err) throw err
+            // console.log(result)
+            res.render(__dirname+'/fetch-data',{data:result})
+            
+        })
+    })
+
+
+}) // here we will be fetching our data by get method
+
+
+
+
+app.get("/delete", (req,res)=>{
+
+    connection.connect(function(err){
+        if(err) throw err
+        let sql = "delete from form where id=?";
+
+        let id = req.query.id;
+        connection.query(sql, [id], function(err, result){
+            if(err) throw err
+            res.redirect('/fetch-data')
+            
+        })
+    })
+
+})
+
 
 
 app.listen(port, ()=> console.log("Connected"))
